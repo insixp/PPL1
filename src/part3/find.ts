ec95f48953599140f31401a1deace8fec8ce4e24
gilevy <gil9@post.bgu.ac.il>
2022-04-04 12:25:06 +0300

    part3

--- src/part3/find.ts
+++ src/part3/find.ts
@@ -1,5 +1,6 @@
 import { Result, makeFailure, makeOk, bind, either } from "../lib/result";
 
+import * as R from "ramda";
 /* Library code */
 const findOrThrow = <T>(pred: (x: T) => boolean, a: T[]): T => {
     for (let i = 0; i < a.length; i++) {
@@ -7,8 +8,9 @@ const findOrThrow = <T>(pred: (x: T) => boolean, a: T[]): T => {
     }
     throw "No element found.";
 }
-console.log("gil levy");
-export const findResult = undefined;
+
+export const findResult = <T>( pred: (x:T)=> boolean,a : T[]): Result<T>=> R.isEmpty(R.filter(pred,a)) ? makeFailure("not found") : makeOk(R.filter(pred,a)[0]) ;
+const square = (x: number): Result<number>=> makeOk(x*x);
 
 /* Client code */
 const returnSquaredIfFoundEven_v1 = (a: number[]): number => {
@@ -20,6 +22,8 @@ const returnSquaredIfFoundEven_v1 = (a: number[]): number => {
     }
 }
 
-export const returnSquaredIfFoundEven_v2 = undefined;
 
-export const returnSquaredIfFoundEven_v3 = undefined;
+
+export const returnSquaredIfFoundEven_v2 = (a: number[]): Result<number> => bind(findResult(x=>x%2==0,a),square);
+
+export const returnSquaredIfFoundEven_v3 = (a :number[]) : number => either(findResult(x=>x%2==0,a),x=>x*x,s=>-1);
